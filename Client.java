997fd7aaddf3fa7ae65784cb3386273d05ff743c
Wiley <bewiley@w13375.davidson.edu>
2015-04-01 22:17:03 -0400

    Added distribution tables.

---
+++ Client.java
@@ -0,0 +1,13 @@
+class Client {
+    final static double[] DIST1 = {0.9, 0.6};
+    final static double[] DIST2 = {0.9, 0.8};
+    final static double[] DIST3 = {0.55, 0.45};
+    final static double[] DIST11 = {0.9, 0.6, 0.6, 0.6, 0.6, 0.6, 0.6, 0.6, 0.6, 0.6};
+    final static double[] DIST12 = {0.9, 0.8, 0.8, 0.8, 0.7, 0.7, 0.7, 0.6, 0.6, 0.6};
+    final static double[] DIST13 = {0.9, 0.8, 0.8, 0.8, 0.8, 0.8, 0.8, 0.8, 0.8, 0.8};
+    final static double[] DIST14 = {0.55, 0.45, 0.45, 0.45, 0.45, 0.45, 0.45, 0.45, 0.45, 0.45};
+    
+    public static void main(String [] args) {
+        
+    }
+}
